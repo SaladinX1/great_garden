dfd85ec02b0a64ae19fd421c3a10b2ef5e3fb03b
SaladinX1 <hopefulhorizon01@gmail.com>
2024-08-14 18:06:12 +0200

    update app.jsx

---
+++ great_garden/src/App.jsx
@@ -0,0 +1,26 @@
+
+import Scene from './components/Scene'
+
+
+function App() {
+ 
+
+  
+
+  return (
+    <>
+    
+
+
+      <div className='min-h-screen w-full relative'>
+        
+      <Scene />
+
+       </div>
+     
+
+    </>
+  )
+}
+
+export default App
